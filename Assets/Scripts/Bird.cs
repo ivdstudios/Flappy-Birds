66b7eadf4110d49499270ed9d756d67d7958e29e
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2017-06-26 09:37:35 +0530

    Final Project

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private bool isDead = false;
+    public float upForce = 200f;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isDead == false)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    private int score = 0;
+    public Text ScoreText;
+
+	// Use this for initialization
+	void Awake () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameOver == true && Input.GetMouseButtonDown(0))
+        {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        ScoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
